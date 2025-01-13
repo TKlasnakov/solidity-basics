8789162b5849b6e99926a05169c63227dd902d96
tklasnakov <t.klasnakov@gmail.com>
2025-01-13 07:32:28 +0200

    Complete functions and arrays, struct lessons

--- smart-contracts/SimpleStorage.sol
+++ smart-contracts/SimpleStorage.sol
@@ -2,6 +2,24 @@
 pragma solidity ^0.8.18;
 
  contract SimpleStorage {
-    uint256 favoirteNumber;
- }  
+    uint256 public myFavoriteNumber;
+    uint256[] listOfNumbers;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(
+            Person({name: _name, favoriteNumber: _favoriteNumber})
+            );
+    }
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+}  
 
